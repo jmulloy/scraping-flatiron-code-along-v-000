6d79d59220ce0073dde32c9bc538495bea3f9c2a
Josh Mulloy <joshmulloy1@gmail.com>
2018-07-12 22:18:26 +0000

    automatically backed up by learn

--- lib/course.rb
+++ lib/course.rb
@@ -6,6 +6,13 @@ attr_accessor :title, :schedule, :description
 
   def initialize
     @@all << self
+    doc.css(".post").each do |post|
+    course = Course.new
+    course.title = post.css("h2").text
+    course.schedule = post.css(".date").text
+    course.description = post.css("p").text
+  end
+
   end
 
   def self.all
